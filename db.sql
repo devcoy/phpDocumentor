2789062515de40727f3aae89cb0a7240b509e4af
Jorge Cervantes <jorgecdev@gmail.com>
2020-05-14 14:00:43 -0500

    estructura de DB actualizada

--- db.sql
+++ db.sql
@@ -5,10 +5,10 @@ USE php_documentor;
 CREATE TABLE IF NOT EXISTS Users(
   id int(255) not null auto_increment,
   name varchar(50) not null,
-  surname varchar(100) not null,
+  surname varchar(100),
   email varchar(50) not null,
-  passwd varchar(50) not null,
-  updated_at datetime not null,
+  password varchar(50) not null,
+  updated_at datetime,
   created_at datetime,
 
   CONSTRAINT pk_users PRIMARY KEY(id)
